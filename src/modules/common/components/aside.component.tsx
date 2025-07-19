b748f8250e4fa4bad6e8107c1462df05102c7958
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-07-19 00:02:58 -0600

    Finished the basic setup of personal information in the Aside component

--- src/modules/common/components/aside.component.tsx
+++ src/modules/common/components/aside.component.tsx
@@ -1,22 +1,35 @@
-// import Image from "next/image";
-import Info from '../../profile/components/info.component';
+import Image from "next/image";
+import Info from "../../profile/components/info.component";
+import GithubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Aside() {
   return (
-    <aside className="flex flex-col w-1/4 px-4 py-5 bg-neutral-900 my-20 rounded-lg">
-        <div className="flex flex-col gap-5 items-center h-1/2">
-          <div className="bg-neutral-800 p-3 rounded-md">
-            {/* <Image src="" alt="Logo" /> */}
-          </div>
-          <h1 className="text-white font-bold text-2xl">Manuel Alejandro Olmedo Rojas</h1>
-          <span className="bg-neutral-800 p-2 rounded-lg">
-            <h2 className="text-white text-sm">Full Stack Web Developer</h2>
-          </span>
+    <aside className="flex flex-col w-1/4 px-10 py-10 bg-neutral-900 my-14 rounded-2xl border border-neutral-800 justify-between">
+      <div className="flex flex-col gap-5 items-center">
+        <div className="bg-gradient-to-br from-neutral-800 to-[#212121] p-8 rounded-[30px]">
+          <Image src="/profile.jpg" alt="Logo" height={500} width={500} className="rounded-full w-full max-h-[100px]" />
         </div>
+        <h1 className="text-white font-bold text-2xl text-center">Manuel Alejandro Olmedo Rojas</h1>
+        <span className="bg-neutral-800 px-4 py-2 rounded-lg">
+          <h2 className="text-white text-sm">Full-Stack Web Developer</h2>
+        </span>
+      </div>
 
-        <div className="flex flex-col items-center gap-5 h-1/2">
-          <Info />
-        </div>
-      </aside> 
-  )
-} 
+      <div className="w-full border-t border-neutral-600"></div>
+
+      <div className="flex flex-col items-center gap-5">
+        <Info type="email" content="xarasho.mail@gmail.com" />
+        <Info type="phone" content="+52 477 865 1898" />
+        <Info type="birthday" content="September 14, 1995" />
+        <Info type="location" content="Salamanca, Guanajuato" />
+      </div>
+      <div className="flex justify-center gap-5">
+        <GithubIcon sx={{ color: 'white' }}/>
+        <LinkedInIcon sx={{ color: 'white' }}/>
+        <InstagramIcon sx={{ color: 'white' }}/>
+      </div>
+    </aside>
+  );
+}
--- src/modules/profile/components/info.component.tsx
+++ src/modules/profile/components/info.component.tsx
@@ -1,14 +1,35 @@
-// import Image from "next/image";
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import PhoneIphoneOutlinedIcon from '@mui/icons-material/PhoneIphoneOutlined';
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
 
-export default function Info() {
+
+interface Props {
+  type: string;
+  content: string
+}
+
+export default function Info({ type, content }: Props) {
+
+  const icon = () => {
+    switch (type) {
+      case 'email': return <MailOutlineOutlinedIcon sx={{ color: '#fef08a' }} />
+      case 'phone': return <PhoneIphoneOutlinedIcon sx={{ color: '#fef08a' }} />
+      case 'birthday': return <CalendarMonthOutlinedIcon sx={{ color: '#fef08a' }} />
+      case 'location': return <FmdGoodOutlinedIcon sx={{ color: '#fef08a' }} />
+      default: 
+        break;
+    }
+  }
+
   return (
     <div className="flex gap-2 w-full">
-      <div className="bg-gradient-to-br from-neutral-700 to-neutral-800 rounded-lg ">
-        {/* <Image src="" alt="icon" /> */}
+      <div className="bg-gradient-to-br from-neutral-800 to-[1a1a1a] rounded-lg p-3">
+        {icon()}
       </div>
       <div>
-        <h1 className="text-neutral-400 text-sm uppercase">Email</h1>
-        <p className="text-white text-sm">xarasho.mail@gmail.com</p>
+        <h1 className="text-neutral-400 text-sm uppercase">{type}</h1>
+        <p className="text-white text-sm">{content}</p>
       </div>
     </div>
   );
